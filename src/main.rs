7446b177c418996830bde9b5a52d95d864c6803e
craig fay <craigfay1@gmail.com>
2020-12-18 18:18:52 -0600

    Create SDL texture from png image

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use sdl2::render::BlendMode;
 
+use image;
+use image::{ImageBuffer, Rgba};
+
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -20,9 +23,16 @@ pub fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
     let texture_creator = canvas.texture_creator();
 
+    let img = image::open("rust.png").unwrap().into_rgba8();
+
     let mut texture = texture_creator
-        .create_texture_streaming(PixelFormatEnum::RGBA32, 256, 256)
+        .create_texture_streaming(
+            PixelFormatEnum::RGBA32,
+            img.width(),
+            img.height(),
+        )
         .map_err(|e| e.to_string())?;
+
 
     // A blend mode needs to be set in order for alpha channels
     // to take effect.
@@ -31,13 +41,16 @@ pub fn main() -> Result<(), String> {
     texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
         // pitch is the number of bytes in a row of pixel data,
         // including padding between lines
-        for y in 0..256 {
-            for x in 0..256 {
-                let offset = y * pitch + x * 4;
-                buffer[offset] = x as u8; // Red
-                buffer[offset + 1] = 0; // Green
-                buffer[offset + 2] = 100; // Blue
-                buffer[offset + 3] = 100; // Alpha
+        for y in 0..img.height() {
+            for x in 0..img.width() {
+
+                let offset: usize = y as usize * pitch + x as usize * 4;
+                let pixel = img.get_pixel(x as u32, y as u32);
+
+                buffer[offset] = pixel[0];
+                buffer[offset + 1] = pixel[1];
+                buffer[offset + 2] = pixel[2];
+                buffer[offset + 3] = pixel[3];
             }
         }
     })?;
@@ -48,7 +61,7 @@ pub fn main() -> Result<(), String> {
     canvas.copy_ex(
         &texture,
         None,
-        Some(Rect::new(200, 100, 256, 256)), // x, y, width, height
+        Some(Rect::new(400, 100, 256, 256)), // x, y, width, height
         0.0, // Rotation
         None,
         false,
